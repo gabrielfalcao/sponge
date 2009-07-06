38ae5724754e6c8e2879eea84ca0d6d98b05c854
Gabriel Falcão <gabriel@nacaolivre.org>
2009-07-05 23:07:53 -0300

    Just took django.core.paginators.* to sponge.controller

--- docs/controller.rst
+++ docs/controller.rst
@@ -33,3 +33,156 @@ Then
  * /images/dog.jpg - would serve the file ``"/home/user/images/dog.jpg"`` as is.
  * /images/crop/200x200/dog.jpg - would serve the file ``"/home/user/images/dog.jpg"`` cropped and resized to 200 x 200 pixels.
  * /images/crop/90x80/another/path/image.jpg - would serve the file ``"/home/user/images/another/path/image.jpg"`` cropped and resized to 90 x 80 pixels.
+
+Paginator
+---------
+
+Sponge took `Django's <http://www.djangoproject.com/>`_ `paginator
+<http://docs.djangoproject.com/en/dev/topics/pagination/#topics-pagination>`_,
+but added a few more unit tests to keep our 100% code coverage policy.
+
+The :class:`Paginator` class has this constructor:
+
+.. class:: Paginator(object_list, per_page, orphans=0, allow_empty_first_page=True)
+
+Required arguments
+------------------
+
+``object_list``
+    A list, tuple, Django ``QuerySet``, or other sliceable object with a
+    ``count()`` or ``__len__()`` method.
+
+``per_page``
+    The maximum number of items to include on a page, not including orphans
+    (see the ``orphans`` optional argument below).
+
+Optional arguments
+------------------
+
+``orphans``
+    The minimum number of items allowed on the last page, defaults to zero.
+    Use this when you don't want to have a last page with very few items.
+    If the last page would normally have a number of items less than or equal
+    to ``orphans``, then those items will be added to the previous page (which
+    becomes the last page) instead of leaving the items on a page by
+    themselves. For example, with 23 items, ``per_page=10``, and
+    ``orphans=3``, there will be two pages; the first page with 10 items and
+    the  second (and last) page with 13 items.
+
+``allow_empty_first_page``
+    Whether or not the first page is allowed to be empty.  If ``False`` and
+    ``object_list`` is  empty, then an ``EmptyPage`` error will be raised.
+
+Methods
+-------
+
+.. method:: Paginator.page(number)
+
+    Returns a :class:`Page` object with the given 1-based index. Raises
+    :exc:`InvalidPage` if the given page number doesn't exist.
+
+Attributes
+----------
+
+.. attribute:: Paginator.count
+
+    The total number of objects, across all pages.
+
+    .. note::
+
+        When determining the number of objects contained in ``object_list``,
+        ``Paginator`` will first try calling ``object_list.count()``. If
+        ``object_list`` has no ``count()`` method, then ``Paginator`` will
+        fallback to using ``object_list.__len__()``. This allows objects, such
+        as Django's ``QuerySet``, to use a more efficient ``count()`` method
+        when available.
+
+.. attribute:: Paginator.num_pages
+
+    The total number of pages.
+
+.. attribute:: Paginator.page_range
+
+    A 1-based range of page numbers, e.g., ``[1, 2, 3, 4]``.
+
+``InvalidPage`` exceptions
+==========================
+
+The ``page()`` method raises ``InvalidPage`` if the requested page is invalid
+(i.e., not an integer) or contains no objects. Generally, it's enough to trap
+the ``InvalidPage`` exception, but if you'd like more granularity, you can trap
+either of the following exceptions:
+
+``PageNotAnInteger``
+    Raised when ``page()`` is given a value that isn't an integer.
+
+``EmptyPage``
+    Raised when ``page()`` is given a valid value but no objects exist on that
+    page.
+
+Both of the exceptions are subclasses of ``InvalidPage``, so you can handle
+them both with a simple ``except InvalidPage``.
+
+
+``Page`` objects
+================
+
+.. class:: Page(object_list, number, paginator):
+
+You usually won't construct :class:`Pages <Page>` by hand -- you'll get them
+using :meth:`Paginator.page`.
+
+
+Methods
+-------
+
+.. method:: Page.has_next()
+
+    Returns ``True`` if there's a next page.
+
+.. method:: Page.has_previous()
+
+    Returns ``True`` if there's a previous page.
+
+.. method:: Page.has_other_pages()
+
+    Returns ``True`` if there's a next *or* previous page.
+
+.. method:: Page.next_page_number()
+
+    Returns the next page number. Note that this is "dumb" and will return the
+    next page number regardless of whether a subsequent page exists.
+
+.. method:: Page.previous_page_number()
+
+    Returns the previous page number. Note that this is "dumb" and will return
+    the previous page number regardless of whether a previous page exists.
+
+.. method:: Page.start_index()
+
+    Returns the 1-based index of the first object on the page, relative to all
+    of the objects in the paginator's list. For example, when paginating a list
+    of 5 objects with 2 objects per page, the second page's :meth:`~Page.start_index`
+    would return ``3``.
+
+.. method:: Page.end_index()
+
+    Returns the 1-based index of the last object on the page, relative to all of
+    the objects in the paginator's list. For example, when paginating a list of
+    5 objects with 2 objects per page, the second page's :meth:`~Page.end_index`
+    would return ``4``.
+
+Attributes
+----------
+
+.. attribute:: Page.object_list
+
+    The list of objects on this page.
+
+.. attribute:: Page.number
+
+    The 1-based page number for this page.
+
+.. attribute:: Page.paginator
+
+    The associated :class:`Paginator` object.
--- tests/unit/test_controller.py
+++ tests/unit/test_controller.py
@@ -20,6 +20,8 @@
 import cherrypy
 
 from mox import Mox
+from nose.tools import assert_equal
+from utils import assert_raises
 from sponge import controller
 
 class TestImageHandler:
@@ -113,3 +115,256 @@ class TestImageHandler:
 
         msg = 'Expected "%s", got %r' % (ret, got)
         assert got == ret, msg
+
+class TestPaginator:
+    def check_paginator(self, params, output):
+        """
+        Helper method that instantiates a controller.Paginator object from the passed
+        params and then checks that its attributes match the passed output.
+        """
+        count, num_pages, page_range = output
+        paginator = controller.Paginator(*params)
+        self.check_attribute('count', paginator, count, params)
+        self.check_attribute('num_pages', paginator, num_pages, params)
+        self.check_attribute('page_range', paginator, page_range, params)
+
+    def check_attribute(self, name, paginator, expected, params):
+        """
+        Helper method that checks a single attribute and gives a nice error
+        message upon test failure.
+        """
+        got = getattr(paginator, name)
+        assert_equal(expected, got,
+            "For '%s', expected %s but got %s.  controller.Paginator parameters were: %s"
+            % (name, expected, got, params))
+
+    def test_paginator(self):
+        """
+        Tests the paginator attributes using varying inputs.
+        """
+        nine = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        ten = nine + [10]
+        eleven = ten + [11]
+        tests = (
+            # Each item is two tuples:
+            #     First tuple is controller.Paginator parameters - object_list, per_page,
+            #         orphans, and allow_empty_first_page.
+            #     Second tuple is resulting controller.Paginator attributes - count,
+            #         num_pages, and page_range.
+            # Ten items, varying orphans, no empty first page.
+            ((ten, 4, 0, False), (10, 3, [1, 2, 3])),
+            ((ten, 4, 1, False), (10, 3, [1, 2, 3])),
+            ((ten, 4, 2, False), (10, 2, [1, 2])),
+            ((ten, 4, 5, False), (10, 2, [1, 2])),
+            ((ten, 4, 6, False), (10, 1, [1])),
+            # Ten items, varying orphans, allow empty first page.
+            ((ten, 4, 0, True), (10, 3, [1, 2, 3])),
+            ((ten, 4, 1, True), (10, 3, [1, 2, 3])),
+            ((ten, 4, 2, True), (10, 2, [1, 2])),
+            ((ten, 4, 5, True), (10, 2, [1, 2])),
+            ((ten, 4, 6, True), (10, 1, [1])),
+            # One item, varying orphans, no empty first page.
+            (([1], 4, 0, False), (1, 1, [1])),
+            (([1], 4, 1, False), (1, 1, [1])),
+            (([1], 4, 2, False), (1, 1, [1])),
+            # One item, varying orphans, allow empty first page.
+            (([1], 4, 0, True), (1, 1, [1])),
+            (([1], 4, 1, True), (1, 1, [1])),
+            (([1], 4, 2, True), (1, 1, [1])),
+            # Zero items, varying orphans, no empty first page.
+            (([], 4, 0, False), (0, 0, [])),
+            (([], 4, 1, False), (0, 0, [])),
+            (([], 4, 2, False), (0, 0, [])),
+            # Zero items, varying orphans, allow empty first page.
+            (([], 4, 0, True), (0, 1, [1])),
+            (([], 4, 1, True), (0, 1, [1])),
+            (([], 4, 2, True), (0, 1, [1])),
+            # Number if items one less than per_page.
+            (([], 1, 0, True), (0, 1, [1])),
+            (([], 1, 0, False), (0, 0, [])),
+            (([1], 2, 0, True), (1, 1, [1])),
+            ((nine, 10, 0, True), (9, 1, [1])),
+            # Number if items equal to per_page.
+            (([1], 1, 0, True), (1, 1, [1])),
+            (([1, 2], 2, 0, True), (2, 1, [1])),
+            ((ten, 10, 0, True), (10, 1, [1])),
+            # Number if items one more than per_page.
+            (([1, 2], 1, 0, True), (2, 2, [1, 2])),
+            (([1, 2, 3], 2, 0, True), (3, 2, [1, 2])),
+            ((eleven, 10, 0, True), (11, 2, [1, 2])),
+            # Number if items one more than per_page with one orphan.
+            (([1, 2], 1, 1, True), (2, 1, [1])),
+            (([1, 2, 3], 2, 1, True), (3, 1, [1])),
+            ((eleven, 10, 1, True), (11, 1, [1])),
+        )
+        for params, output in tests:
+            self.check_paginator(params, output)
+
+    def check_indexes(self, params, page_num, indexes):
+        """
+        Helper method that instantiates a controller.Paginator object from the passed
+        params and then checks that the start and end indexes of the passed
+        page_num match those given as a 2-tuple in indexes.
+        """
+        paginator = controller.Paginator(*params)
+        if page_num == 'first':
+            page_num = 1
+        elif page_num == 'last':
+            page_num = paginator.num_pages
+        page = paginator.page(page_num)
+        start, end = indexes
+        msg = ("For %s of page %s, expected %s but got %s."
+               " controller.Paginator parameters were: %s")
+        assert_equal(start, page.start_index(),
+            msg % ('start index', page_num, start, page.start_index(), params))
+        assert_equal(end, page.end_index(),
+            msg % ('end index', page_num, end, page.end_index(), params))
+
+    def test_page_indexes(self):
+        """
+        Tests that paginator pages have the correct start and end indexes.
+        """
+        ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        tests = (
+            # Each item is three tuples:
+            #     First tuple is controller.Paginator parameters - object_list, per_page,
+            #         orphans, and allow_empty_first_page.
+            #     Second tuple is the start and end indexes of the first page.
+            #     Third tuple is the start and end indexes of the last page.
+            # Ten items, varying per_page, no orphans.
+            ((ten, 1, 0, True), (1, 1), (10, 10)),
+            ((ten, 2, 0, True), (1, 2), (9, 10)),
+            ((ten, 3, 0, True), (1, 3), (10, 10)),
+            ((ten, 5, 0, True), (1, 5), (6, 10)),
+            # Ten items, varying per_page, with orphans.
+            ((ten, 1, 1, True), (1, 1), (9, 10)),
+            ((ten, 1, 2, True), (1, 1), (8, 10)),
+            ((ten, 3, 1, True), (1, 3), (7, 10)),
+            ((ten, 3, 2, True), (1, 3), (7, 10)),
+            ((ten, 3, 4, True), (1, 3), (4, 10)),
+            ((ten, 5, 1, True), (1, 5), (6, 10)),
+            ((ten, 5, 2, True), (1, 5), (6, 10)),
+            ((ten, 5, 5, True), (1, 10), (1, 10)),
+            # One item, varying orphans, no empty first page.
+            (([1], 4, 0, False), (1, 1), (1, 1)),
+            (([1], 4, 1, False), (1, 1), (1, 1)),
+            (([1], 4, 2, False), (1, 1), (1, 1)),
+            # One item, varying orphans, allow empty first page.
+            (([1], 4, 0, True), (1, 1), (1, 1)),
+            (([1], 4, 1, True), (1, 1), (1, 1)),
+            (([1], 4, 2, True), (1, 1), (1, 1)),
+            # Zero items, varying orphans, allow empty first page.
+            (([], 4, 0, True), (0, 0), (0, 0)),
+            (([], 4, 1, True), (0, 0), (0, 0)),
+            (([], 4, 2, True), (0, 0), (0, 0)),
+        )
+        for params, first, last in tests:
+            self.check_indexes(params, 'first', first)
+            self.check_indexes(params, 'last', last)
+        # When no items and no empty first page, we should get controller.EmptyPage error.
+        assert_raises(controller.EmptyPage, self.check_indexes, ([], 4, 0, False), 1, None)
+        assert_raises(controller.EmptyPage, self.check_indexes, ([], 4, 1, False), 1, None)
+        assert_raises(controller.EmptyPage, self.check_indexes, ([], 4, 2, False), 1, None)
+
+    def test_representation(self):
+        class PaginatorStub:
+            num_pages = 10
+
+        page = controller.Page([], 2, PaginatorStub)
+        assert_equal('<Page 2 of 10>', repr(page))
+
+    def test_has_next_true(self):
+        class PaginatorStub:
+            num_pages = 10
+
+        page = controller.Page([], 2, PaginatorStub)
+        message = '%s should have next page, since is the page 2 of 10' % page
+        assert page.has_next(), message
+
+    def test_has_next_false(self):
+        class PaginatorStub:
+            num_pages = 2
+
+        page = controller.Page([], 2, PaginatorStub)
+        message = '%s should not have next page, since is the page 2 of 2' % page
+        assert not page.has_next(), message
+
+    def test_has_previous_true(self):
+        class PaginatorStub:
+            num_pages = 10
+
+        page = controller.Page([], 2, PaginatorStub)
+        message = '%s should have previous page, since is the page 2' % page
+        assert page.has_previous(), message
+
+    def test_has_previous_false(self):
+        class PaginatorStub:
+            num_pages = 2
+
+        page = controller.Page([], 1, PaginatorStub)
+        message = '%s should not have previous page, since is the page 1' % page
+        assert not page.has_previous(), message
+
+    def test_has_other_pages_true_1of2(self):
+        class PaginatorStub:
+            num_pages = 2
+
+        page = controller.Page([], 1, PaginatorStub)
+        message = '%s should have other pages' % page
+        assert page.has_other_pages(), message
+
+    def test_has_other_pages_true_2of2(self):
+        class PaginatorStub:
+            num_pages = 2
+
+        page = controller.Page([], 2, PaginatorStub)
+        message = '%s should have other pages' % page
+        assert page.has_other_pages(), message
+
+    def test_has_other_pages_true_2of3(self):
+        class PaginatorStub:
+            num_pages = 3
+
+        page = controller.Page([], 2, PaginatorStub)
+        message = '%s should have other pages' % page
+        assert page.has_other_pages(), message
+
+    def test_has_other_pages_true_3of3(self):
+        class PaginatorStub:
+            num_pages = 3
+
+        page = controller.Page([], 3, PaginatorStub)
+        message = '%s should have other pages' % page
+        assert page.has_other_pages(), message
+
+    def test_has_other_pages_false(self):
+        class PaginatorStub:
+            num_pages = 1
+
+        page = controller.Page([], 1, PaginatorStub)
+        message = '%s should not have other pages' % page
+        assert not page.has_other_pages(), message
+
+    def test_next_page_number(self):
+        class PaginatorStub:
+            num_pages = 2
+
+        page = controller.Page([], 1, PaginatorStub)
+
+        got = page.next_page_number()
+        expected = 2
+
+        message = "%s's next page number should be 2, got %r" % (page, got)
+        assert got == expected, message
+
+    def test_previous_page_number(self):
+        class PaginatorStub:
+            num_pages = 2
+
+        page = controller.Page([], 2, PaginatorStub)
+
+        got = page.previous_page_number()
+        expected = 1
+
+        message = "%s's previous page number should be 1, got %r" % (page, got)
+        assert got == expected, message
