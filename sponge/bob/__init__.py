50a6391cd711008f0af1770f605308796ed5c765
Gabriel Falcão <gabriel@nacaolivre.org>
2009-08-17 10:55:23 -0300

    Updating license headers

--- sponge/bob/__init__.py
+++ sponge/bob/__init__.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-# -*- coding: utf-8; -*-
-#
+# -*- coding: utf-8 -*-
+# <Sponge - Lightweight Web Framework>
 # Copyright (C) 2009 Gabriel Falcão <gabriel@nacaolivre.org>
 # Copyright (C) 2009 Bernardo Heynemann <heynemann@gmail.com>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public
-# License along with this program; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
 import sys
@@ -53,6 +51,8 @@ class Bob(object):
     """Sponge Bob is the responsible for managing
     the user's application and its modules."""
 
+    ACTIONS = 'create', 'go', 'start'
+
     def __init__(self, parser=None, fs=None):
         self.parser = parser
         if not self.parser:
@@ -73,11 +73,9 @@ class Bob(object):
         options, args = self.parser.parse_args()
         error_msg = '\nBob got a error when %s.\n    %s\n'
 
-        accepted = 'create', 'go', 'start'
-
         if not args:
             msg = '\nmissing argument, choose one in %s\n'
-            sys.stderr.write(msg % ", ".join(accepted))
+            sys.stderr.write(msg % ", ".join(self.ACTIONS))
             self.exit()
 
         if args[0] not in accepted:
